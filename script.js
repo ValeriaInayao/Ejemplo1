9fe0e3be3ff036c33e026b81d8e0778604cd4743
valeria <valeria.inayao.mancilla@institutocomercialosorno.cl>
2024-04-03 13:33:02 -0400

    Primer commit

---
+++ script.js
@@ -0,0 +1,17 @@
+function aumentarLike1(){
+    var contador1 = document.getElementById("megusta1");
+    var like1 = parseInt(contador1.innerHTML);
+    contador1.innerHTML = like1 + 1 + "like(s)"
+}
+
+function aumentarLike2(){
+    var contador2 = document.getElementById("megusta2");
+    var like2 = parseInt(contador2.innerHTML);
+    contador2.innerHTML = like2 + 1 + "like(s)"
+}
+
+function aumentarLike3(){
+    var contador3 = document.getElementById("megusta3");
+    var like3 = parseInt(contador3.innerHTML);
+    contador3.innerHTML = like3 + 1 + "like(s)"
+}
